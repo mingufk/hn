b3991997944f0b5d0df171549cdc87275a9eddab
Mingu Kim <mingufk@gmail.com>
2023-03-02 21:50:12 +0900

    ➕ tailwindcss

--- main.js
+++
@@ -1,7 +0,0 @@
-import "./style.css";
-
-document.querySelector("#app").innerHTML = /*html*/ `
-  <div>
-    <h1>Hacker News</h1>
-  </div>
-`;
---
+++ src/main.js
@@ -0,0 +1,7 @@
+import "./main.css";
+
+document.querySelector("#app").innerHTML = /*html*/ `
+  <div>
+    <h1 class="text-3xl font-bold underline">Hacker News</h1>
+  </div>
+`;
